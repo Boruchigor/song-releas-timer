6c236478f7a59f9bb23560c4b63c3b02236d62dd
Duckslord <boruchigor@gmail.com>
2024-08-14 20:26:29 +0200

    ss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script>
 	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
-	import { webVitals } from '$lib/vitals';
-	import Header from './Header.svelte';
 	import './styles.css';
 
 	/** @type {import('./$types').LayoutServerData} */
