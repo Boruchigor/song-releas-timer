88bbb788a06f8b81b39815a8aec38b17161399fb
Duckslord <boruchigor@gmail.com>
2024-08-15 18:50:39 +0200

    gdgfdg

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,12 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+	  adapter: adapter(),
+	  paths: {
+		assets: '',
+		base: '',
+	  }
 	}
-};
+  };  
 
 export default config;
